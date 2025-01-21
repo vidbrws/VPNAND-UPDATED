64666ca11c17fe27b9650e3923951acc31874e08
Mikolaj Dobrucki <mikolaj.dobrucki@gmail.com>
2025-01-21 22:25:53 +0100

    clean up section classes

--- components/ui/section.tsx
+++ components/ui/section.tsx
@@ -9,8 +9,8 @@ const Section = React.forwardRef<
   <section
     ref={ref}
     className={cn(
-      "bg-background text-foreground py-12 sm:py-24 md:py-32 px-4",
-      className
+      "bg-background px-4 py-12 text-foreground sm:py-24 md:py-32",
+      className,
     )}
     {...props}
   />
