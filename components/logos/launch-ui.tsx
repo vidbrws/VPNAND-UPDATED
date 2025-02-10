80df045df8c4521efb2a62840d4bea2a5b58fe56
Mikolaj Dobrucki <mikolaj.dobrucki@gmail.com>
2025-02-10 10:12:35 +0100

    new logo

--- components/logos/launch-ui.tsx
+++ components/logos/launch-ui.tsx
@@ -8,29 +8,12 @@ const LaunchUI = (props: React.SVGProps<SVGSVGElement>) => (
     {...props}
   >
     <path
-      d="M13.205 15.1599L8.96234 10.9173L5.25003 10.3869L8.96234 6.67462L12.6747 7.20495L15.3263 4.5533C16.9173 2.96231 20.6296 3.49264 20.6296 3.49264C20.6296 3.49264 21.1599 7.20495 19.5689 8.79594L16.9173 11.4476L17.4476 15.1599L13.7353 18.8722L13.205 15.1599Z"
-      fill="url(#paint0_linear_1_2990)"
+      d="M10.5 12.75H3L6 9.75H9L15.75 3H20.25L10.5 12.75Z"
+      fill="currentColor"
     />
-    <path
-      d="M8.96234 10.9173L13.205 15.1599M8.96234 10.9173L5.25003 10.3869L8.96234 6.67462L12.6747 7.20495M8.96234 10.9173L12.6747 7.20495M13.205 15.1599L13.7353 18.8722L17.4476 15.1599L16.9173 11.4476M13.205 15.1599L16.9173 11.4476M16.9173 11.4476C16.9173 11.4476 17.978 10.3869 19.5689 8.79594C21.1599 7.20495 20.6296 3.49264 20.6296 3.49264C20.6296 3.49264 16.9173 2.96231 15.3263 4.5533C13.7353 6.14429 12.6747 7.20495 12.6747 7.20495M8.25003 14.25L3.00003 19.5M9.00003 17.25L6.00003 20.25"
-      stroke="currentColor"
-      strokeWidth={2}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <defs>
-      <linearGradient
-        id="paint0_linear_1_2990"
-        x1={3.00003}
-        y1={8.25}
-        x2={10.0952}
-        y2={20.6917}
-        gradientUnits="userSpaceOnUse"
-      >
-        <stop stopColor="currentColor" stopOpacity={0.6} />
-        <stop offset={1} stopColor="currentColor" stopOpacity={0} />
-      </linearGradient>
-    </defs>
+    <path d="M11.25 15V13.5L21 3.75V5.25L11.25 15Z" fill="currentColor" />
+    <path d="M11.25 18V16.5L21 6.75V8.25L11.25 18Z" fill="currentColor" />
+    <path d="M11.25 21V19.5L15 15.75V17.25L11.25 21Z" fill="currentColor" />
   </svg>
 );
 export default LaunchUI;
