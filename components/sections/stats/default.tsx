efd6619774af52a3c2d436dfcad9bac6538463c6
Mikolaj Dobrucki <mikolaj.dobrucki@gmail.com>
2025-03-08 08:50:20 +0100

    tailwind 4: fix color functions

--- components/sections/stats/default.tsx
+++ components/sections/stats/default.tsx
@@ -7,54 +7,54 @@ export default function Stats() {
       <div className="container mx-auto max-w-[960px]">
         <div className="grid grid-cols-2 gap-12 sm:grid-cols-4">
           <div className="flex flex-col items-start gap-3 text-left">
-            <div className="text-sm font-semibold text-muted-foreground">
+            <div className="text-muted-foreground text-sm font-semibold">
               used by
             </div>
             <div className="flex items-baseline gap-2">
-              <div className="bg-linear-to-r from-foreground to-foreground bg-clip-text text-4xl font-medium text-transparent drop-shadow-[2px_1px_24px_hsla(var(--brand-foreground))] transition-all duration-300 sm:text-5xl md:text-6xl dark:to-brand">
+              <div className="from-foreground to-foreground dark:to-brand bg-linear-to-r bg-clip-text text-4xl font-medium text-transparent drop-shadow-[2px_1px_24px_var(--brand-foreground)] transition-all duration-300 sm:text-5xl md:text-6xl">
                 {Math.round(siteConfig.stats.figma / 100) / 10}
               </div>
-              <div className="text-2xl font-semibold text-brand">k</div>
+              <div className="text-brand text-2xl font-semibold">k</div>
             </div>
-            <div className="text-pretty text-sm font-semibold text-muted-foreground">
+            <div className="text-muted-foreground text-sm font-semibold text-pretty">
               designers on Figma Community
             </div>
           </div>
           <div className="flex flex-col items-start gap-3 text-left">
-            <div className="text-sm font-semibold text-muted-foreground">
+            <div className="text-muted-foreground text-sm font-semibold">
               over
             </div>
             <div className="flex items-baseline gap-2">
-              <div className="bg-linear-to-r from-foreground to-foreground bg-clip-text text-4xl font-medium text-transparent drop-shadow-[2px_1px_24px_hsla(var(--brand-foreground))] transition-all duration-300 sm:text-5xl md:text-6xl dark:to-brand">
+              <div className="from-foreground to-foreground dark:to-brand bg-linear-to-r bg-clip-text text-4xl font-medium text-transparent drop-shadow-[2px_1px_24px_var(--brand-foreground)] transition-all duration-300 sm:text-5xl md:text-6xl">
                 {siteConfig.stats.github}
               </div>
             </div>
-            <div className="text-pretty text-sm font-semibold text-muted-foreground">
+            <div className="text-muted-foreground text-sm font-semibold text-pretty">
               clones and forks of the template on Github
             </div>
           </div>
           <div className="flex flex-col items-start gap-3 text-left">
-            <div className="text-sm font-semibold text-muted-foreground">
+            <div className="text-muted-foreground text-sm font-semibold">
               already
             </div>
             <div className="flex items-baseline gap-2">
-              <div className="bg-linear-to-r from-foreground to-foreground bg-clip-text text-4xl font-medium text-transparent drop-shadow-[2px_1px_24px_hsla(var(--brand-foreground))] transition-all duration-300 sm:text-5xl md:text-6xl dark:to-brand">
+              <div className="from-foreground to-foreground dark:to-brand bg-linear-to-r bg-clip-text text-4xl font-medium text-transparent drop-shadow-[2px_1px_24px_var(--brand-foreground)] transition-all duration-300 sm:text-5xl md:text-6xl">
                 {Math.round(siteConfig.stats.cli / 100) / 10}
               </div>
-              <div className="text-2xl font-semibold text-brand">k</div>
+              <div className="text-brand text-2xl font-semibold">k</div>
             </div>
-            <div className="text-pretty text-sm font-semibold text-muted-foreground">
+            <div className="text-muted-foreground text-sm font-semibold text-pretty">
               installations with shadcn/ui CLI
             </div>
           </div>
           <div className="flex flex-col items-start gap-3 text-left">
-            <div className="text-sm font-semibold text-muted-foreground">
+            <div className="text-muted-foreground text-sm font-semibold">
               includes
             </div>
-            <div className="bg-linear-to-r from-foreground to-foreground bg-clip-text text-4xl font-medium text-transparent drop-shadow-[2px_1px_24px_hsla(var(--brand-foreground))] transition-all duration-300 sm:text-5xl md:text-6xl dark:to-brand">
+            <div className="from-foreground to-foreground dark:to-brand bg-linear-to-r bg-clip-text text-4xl font-medium text-transparent drop-shadow-[2px_1px_24px_var(--brand-foreground)] transition-all duration-300 sm:text-5xl md:text-6xl">
               {siteConfig.stats.components}
             </div>
-            <div className="text-pretty text-sm font-semibold text-muted-foreground">
+            <div className="text-muted-foreground text-sm font-semibold text-pretty">
               React components and sections
             </div>
           </div>
