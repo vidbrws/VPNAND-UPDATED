152a92c0efe5f12fa7051e435b1db82d367860b3
Mikolaj Dobrucki <mikolaj.dobrucki@gmail.com>
2025-03-12 07:45:59 +0100

    fix sheet border

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -32,7 +32,7 @@ function SheetOverlay({
 }
 
 const sheetVariants = cva(
-  "fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500 data-[state=open]:animate-in data-[state=closed]:animate-out",
+  "border-border dark:border-border/15 fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500 data-[state=open]:animate-in data-[state=closed]:animate-out",
   {
     variants: {
       side: {
