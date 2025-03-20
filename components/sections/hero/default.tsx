17bc7a7c9ee4a887d60322de55e0f3c0f77e47d6
Mikolaj Dobrucki <mikolaj.dobrucki@gmail.com>
2025-03-20 09:36:50 +0100

    layout improvements to hero

--- components/sections/hero/default.tsx
+++ components/sections/hero/default.tsx
@@ -7,7 +7,7 @@ import Glow from "../../ui/glow";
 import { siteConfig } from "@/config/site";
 import Github from "../../logos/github";
 import { ReactNode } from "react";
-import MainMockup from "../../mockups/main";
+import Screenshot from "../../ui/screenshot";
 
 interface HeroButtonProps {
   href: string;
@@ -28,7 +28,15 @@ interface HeroProps {
 export default function Hero({
   title = "Give your big idea the website it deserves",
   description = "Landing page components built with React, Shadcn/ui and Tailwind that will make your website feel premium.",
-  mockup,
+  mockup = (
+    <Screenshot
+      srcLight="/app-light.png"
+      srcDark="/app-dark.png"
+      alt="Launch UI app screenshot"
+      width={1248}
+      height={765}
+    />
+  ),
   badge = (
     <Badge variant="outline" className="animate-appear">
       <span className="text-muted-foreground">
@@ -59,10 +67,10 @@ export default function Hero({
       <div className="max-w-container mx-auto flex flex-col gap-12 pt-16 sm:gap-24">
         <div className="flex flex-col items-center gap-6 text-center sm:gap-12">
           {badge !== false && badge}
-          <h1 className="animate-appear from-foreground to-foreground dark:to-muted-foreground relative z-10 inline-block bg-linear-to-r bg-clip-text text-4xl leading-tight font-semibold text-transparent drop-shadow-2xl sm:text-6xl sm:leading-tight md:text-8xl md:leading-tight">
+          <h1 className="animate-appear from-foreground to-foreground dark:to-muted-foreground relative z-10 inline-block bg-linear-to-r bg-clip-text text-4xl leading-tight font-semibold text-balance text-transparent drop-shadow-2xl sm:text-6xl sm:leading-tight md:text-8xl md:leading-tight">
             {title}
           </h1>
-          <p className="text-md animate-appear text-muted-foreground relative z-10 max-w-[550px] font-medium opacity-0 delay-100 sm:text-xl">
+          <p className="text-md animate-appear text-muted-foreground relative z-10 font-medium text-balance opacity-0 delay-100 sm:text-xl">
             {description}
           </p>
           {buttons !== false && buttons.length > 0 && (
@@ -89,8 +97,11 @@ export default function Hero({
                 className="animate-appear opacity-0 delay-700"
                 size="small"
               >
-                <Mockup type="responsive" className="w-full">
-                  {mockup || <MainMockup />}
+                <Mockup
+                  type="responsive"
+                  className="bg-background w-full rounded-xl border-0"
+                >
+                  {mockup}
                 </Mockup>
               </MockupFrame>
               <Glow
