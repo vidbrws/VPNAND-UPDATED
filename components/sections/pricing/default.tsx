9b6356a9eab71fecb01e2b35b94251637683ec16
Mikolaj Dobrucki <mikolaj.dobrucki@gmail.com>
2025-03-21 14:27:43 +0100

    fix pricing section

--- components/sections/pricing/default.tsx
+++ components/sections/pricing/default.tsx
@@ -75,21 +75,20 @@ export default function Pricing({
   return (
     <Section className={cn(className)}>
       <div className="mx-auto flex max-w-6xl flex-col items-center gap-12">
-        {title ||
-          (description && (
-            <div className="flex flex-col items-center gap-4 px-4 text-center sm:gap-8">
-              {title && (
-                <h2 className="text-3xl leading-tight font-semibold sm:text-5xl sm:leading-tight">
-                  {title}
-                </h2>
-              )}
-              {description && (
-                <p className="text-md text-muted-foreground max-w-[600px] font-medium sm:text-xl">
-                  {description}
-                </p>
-              )}
-            </div>
-          ))}
+        {(title || description) && (
+          <div className="flex flex-col items-center gap-4 px-4 text-center sm:gap-8">
+            {title && (
+              <h2 className="text-3xl leading-tight font-semibold sm:text-5xl sm:leading-tight">
+                {title}
+              </h2>
+            )}
+            {description && (
+              <p className="text-md text-muted-foreground max-w-[600px] font-medium sm:text-xl">
+                {description}
+              </p>
+            )}
+          </div>
+        )}
         {plans !== false && plans.length > 0 && (
           <div className="max-w-container mx-auto grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
             {plans.map((plan) => (
