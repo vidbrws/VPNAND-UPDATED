25b323dc6c196d97c6c53d3e58ad64db0d69e232
Mikolaj Dobrucki <mikolaj.dobrucki@gmail.com>
2025-03-21 09:22:11 +0100

    fix hero description max width

--- components/sections/hero/default.tsx
+++ components/sections/hero/default.tsx
@@ -70,7 +70,7 @@ export default function Hero({
           <h1 className="animate-appear from-foreground to-foreground dark:to-muted-foreground relative z-10 inline-block bg-linear-to-r bg-clip-text text-4xl leading-tight font-semibold text-balance text-transparent drop-shadow-2xl sm:text-6xl sm:leading-tight md:text-8xl md:leading-tight">
             {title}
           </h1>
-          <p className="text-md animate-appear text-muted-foreground relative z-10 font-medium text-balance opacity-0 delay-100 sm:text-xl">
+          <p className="text-md animate-appear text-muted-foreground relative z-10 max-w-[740px] font-medium text-balance opacity-0 delay-100 sm:text-xl">
             {description}
           </p>
           {buttons !== false && buttons.length > 0 && (
@@ -99,7 +99,7 @@ export default function Hero({
               >
                 <Mockup
                   type="responsive"
-                  className="bg-background w-full rounded-xl border-0"
+                  className="bg-background/90 w-full rounded-xl border-0"
                 >
                   {mockup}
                 </Mockup>
