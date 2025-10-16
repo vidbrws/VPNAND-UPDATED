6ba690fdc441c695be928da41906744e8e1345d4
Victoria D <vdvbws@gmail.com>
2025-10-17 00:22:03 +0600

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,7 @@ import Hero from "../components/sections/hero/default";
 import Logos from "../components/sections/logos/default";
 import Navbar from "../components/sections/navbar/default";
 import Pricing from "../components/sections/pricing/default";
-import Stats from "../components/sections/stats/default";
 import {Install} from "../components/sections/phone_steps/Install";
-import {RevealCard} from "../components/sections/reveal-card/default";
 import  {MarqueeDemo} from "../components/sections/reviews/default";
 import Items from "../components/sections/items/default";
 
